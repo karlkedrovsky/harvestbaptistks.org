3c1e8293f72a9974434f838547cf6a4a0cfebfad
Karl Kedrovsky <karl@kedrovsky.com>
2011-04-03 13:26:30 -0500

    added new screenshot, context module and updated styles

--- htdocs/sites/all/themes/harvestbaptistks/template.php
+++ htdocs/sites/all/themes/harvestbaptistks/template.php
@@ -1,7 +1,13 @@
 <?php
 function harvestbaptistks_preprocess_node(&$variables) {
+  $u = user_load($variables['uid']);
+  if (!empty($u->first_name) || !empty($u->last_name)) {
+    $name = $u->first_name . ' ' . $u->last_name;
+  } else {
+    $name = $variables['name'];
+  }
   if ($variables['teaser']) {
     $variables['submitted'] = t('Submitted by<br/>!username<br/>!datetime',
-                              array('!username' => $variables['name'], '!datetime' => format_date($variables['created'], 'short')));
+                              array('!username' => $name, '!datetime' => format_date($variables['created'], 'short')));
   }
 }
