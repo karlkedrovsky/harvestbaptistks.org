e7958ebb9535897bde45e4a64098331380a7b7c3
Karl Kedrovsky <karl@kedrovsky.com>
2011-04-02 21:09:04 -0500

    updated submitted block

--- htdocs/sites/all/themes/harvestbaptistks/template.php
+++ htdocs/sites/all/themes/harvestbaptistks/template.php
@@ -1,13 +1,7 @@
 <?php
 function harvestbaptistks_preprocess_node(&$variables) {
-  //if ($variables['teaser']) {
-   $variables['submitted'] = t('Submitted by<br/>!username<br/>!datetime', array('!username' => $variables['name'], '!datetime' => $variables['date']));
-  //}
+  if ($variables['teaser']) {
+    $variables['submitted'] = t('Submitted by<br/>!username<br/>!datetime',
+                              array('!username' => $variables['name'], '!datetime' => format_date($variables['created'], 'short')));
+  }
 }
-
-// function bartik_preprocess_node(&$variables) {
-//   $variables['submitted'] = t('published by !username on !datetime', array('!username' => $variables['name'], '!datetime' => $variables['date']));
-//   if ($variables['view_mode'] == 'full' && node_is_page($variables['node'])) {
-//     $variables['classes_array'][] = 'node-full';
-//   }
-// }
